a8f385ae187c6a5ffdfa8712d455a8090b4cdc2a
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-07-24 19:27:25 -0500

    Initial commit for real this time

---
+++ Source/RV2-Esegn-CPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimVore2;
+using Verse;
+
+namespace RV2_Esegn_CPI
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup()
+        {
+            Harmony.DEBUG = false;
+            Harmony harmony = new Harmony("RV2_Esegn_CPI");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            
+            RV2Log.Message("TEST - Cross-path interactions loaded!");
+        }
+    }
+}
